1076050f4d5c7f69eacf236b79321607da14fa92
emmanuel <emmanuel.kisoso@student.moringaschool.com>
2024-03-19 12:15:53 +0300

    Intial Commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1=2;
+let num2=31;
+let multiply= num1*num2
+console.log (multiply);
 
-// Write your code here
+let random =87
+Number.isInteger(87)
+
+let num3=24
+let num4=5
+let mod= num3%num4
+console.log(mod)
+
+let max=Math.max(15,16,17,18,19,20);
+console.log(max)
